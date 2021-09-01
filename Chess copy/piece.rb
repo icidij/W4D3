4aa3e8784e9d12bea8cd5aa7119fe7bbd4100ac7
icidij <jamesc1288@gmail.com>
2021-09-01 14:11:19 -0400

    grow_unblocked_moves_in_dir progress

--- Chess copy/piece.rb
+++ Chess copy/piece.rb
@@ -1,5 +1,5 @@
 class Piece
-  attr_reader :color
+  attr_reader :color, :pos
 
   def initialize(color, board, pos)
     @color = color
--- Error Handling Funtime/super_useful.rb
+++ Error Handling Funtime/super_useful.rb
@@ -8,11 +8,13 @@ end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
-
+class CoffeeError < StandardError; end
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
+  elsif maybe_fruit.downcase == "coffee" 
+    raise CoffeeError
+  else
     raise StandardError 
   end 
 end
@@ -24,20 +26,37 @@ def feed_me_a_fruit
   begin
   maybe_fruit = gets.chomp
   reaction(maybe_fruit) 
-  rescue
-  puts "I don't like that fruit, try another"
-  retry
+  rescue CoffeeError
+    puts "Thanks for the coffee, but I want fruit"
+    retry
   end
 end  
 
 # PHASE 4
+class YearError < ArgumentError; end
+class StringLengthError < ArgumentError; end
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
-    @name = name
-    @yrs_known = yrs_known
-    @fav_pastime = fav_pastime
+    # @name = name
+    if yrs_known < 5
+      raise YearError.new("That's not long enough, we will wait")
+    else
+      @yrs_known = yrs_known
+    end
+    begin
+    if (fav_pastime.length <= 0) || (name.length <= 0)
+      raise StringLengthError.new("We need a pastime and a name") 
+    else
+      @fav_pastime = fav_pastime
+    end
+  rescue YearError => e
+    @yrs_known = 5
+  rescue StringLengthError => e
+    @name = ""
+    @fav_pastime = ""
+ 
   end
-
+end
   def talk_about_friendship
     puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
   end
